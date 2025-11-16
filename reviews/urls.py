540654e310a01c4034f8511026de8cd4f776f085
Jemimah Jemutai <jemutaijemimah@gmail.com>
2025-11-16 22:30:27 +0300

    Add CRUD URLs to reviews

--- reviews/urls.py
+++ reviews/urls.py
@@ -6,4 +6,6 @@ app_name = 'reviews'
 urlpatterns = [
     path('', views.review_list, name='review_list'),
     path('create/<int:service_id>/', views.review_create, name='review_create'),
+        path('<int:pk>/update/', views.review_update, name='review_update'),
+    path('<int:pk>/delete/', views.review_delete, name='review_delete'),
 ]
