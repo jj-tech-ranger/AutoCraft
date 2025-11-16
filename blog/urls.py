0040b0be57092c248906783ab890d17f7da9ee63
Jemimah Jemutai <jemutaijemimah@gmail.com>
2025-11-16 22:38:47 +0300

    Add CRUD URLs to blog

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,7 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.blog_list, name='blog_list'),
     path('<slug:slug>/', views.blog_detail, name='blog_detail'),
+        path('create/', views.blog_post_create, name='blog_post_create'),
+    path('<slug:slug>/update/', views.blog_post_update, name='blog_post_update'),
+    path('<slug:slug>/delete/', views.blog_post_delete, name='blog_post_delete'),
 ]
