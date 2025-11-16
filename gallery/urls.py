eda166df69aa168c32fb43c5adc250b00a6dbfd8
Jemimah Jemutai <jemutaijemimah@gmail.com>
2025-11-16 22:28:24 +0300

    Add CRUD URLs to gallery

--- gallery/urls.py
+++ gallery/urls.py
@@ -6,4 +6,7 @@ app_name = 'gallery'
 urlpatterns = [
     path('', views.gallery_list, name='gallery_list'),
     path('<int:pk>/', views.gallery_detail, name='gallery_detail'),
+        path('create/', views.gallery_image_create, name='gallery_image_create'),
+    path('<int:pk>/update/', views.gallery_image_update, name='gallery_image_update'),
+    path('<int:pk>/delete/', views.gallery_image_delete, name='gallery_image_delete'),
 ]
