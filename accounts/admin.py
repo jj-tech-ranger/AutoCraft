ea3eb37bb90e82ce849f8883898ea25f0ff1678d
Jemimah Jemutai <jemutaijemimah@gmail.com>
2025-11-16 14:22:02 +0300

    Create accounts admin.py
    Django admin configuration for CustomUser model.

---
+++ accounts/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['email', 'first_name', 'last_name', 'role', 'is_active', 'date_joined']
+    list_filter = ['role', 'is_active', 'is_staff']
+    search_fields = ['email', 'first_name', 'last_name']
+    ordering = ['-date_joined']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal Info', {'fields': ('first_name', 'last_name', 'phone_number')}),
+        ('Permissions', {'fields': ('role', 'is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'first_name', 'last_name', 'password1', 'password2', 'role', 'is_active', 'is_staff'),
+        }),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
