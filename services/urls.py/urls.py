b7389bd744a05ead437660da07ff9f72edc703f1
Jemimah Jemutai <jemutaijemimah@gmail.com>
2025-11-16 22:18:24 +0300

    Update urls.py

--- services/urls.py/urls.py
+++ services/urls.py/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('', views.service_list, name='service_list'),
     path('<slug:slug>/', views.service_detail, name='service_detail'),
     path('category/<slug:slug>/', views.category_services, name='category_services'),
+        path('create/', views.service_create, name='service_create'),
+    path('<int:pk>/update/', views.service_update, name='service_update'),
+    path('<int:pk>/delete/', views.service_delete, name='service_delete'),
 ]
