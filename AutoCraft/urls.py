f18f90eb280011789006decc06ac7f3febfbef93
Jemimah Jemutai <jemutaijemimah@gmail.com>
2025-11-16 18:15:10 +0300

    Add URL patterns for new apps (services, bookings, gallery, reviews, blog)

--- AutoCraft/urls.py
+++ AutoCraft/urls.py
@@ -6,5 +6,10 @@ from django.conf.urls import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('auto.urls')),
+        path('services/', include('services.urls')),
+        path('bookings/', include('bookings.urls')),
+        path('gallery/', include('gallery.urls')),
+        path('reviews/', include('reviews.urls')),
+        path('blog/', include('blog.urls')),
 
 ]
