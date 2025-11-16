63f1b5e6120a1ce9f74902050b87ff1a4bb2978d
Jemimah Jemutai <jemutaijemimah@gmail.com>
2025-11-16 16:03:35 +0300

    Create models.py

---
+++ services/models.py/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+from django.utils.text import slugify
+
+class ServiceCategory(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True, blank=True)
+    description = models.TextField(blank=True)
+    icon = models.CharField(max_length=50, blank=True)
+    is_active = models.BooleanField(default=True)
+    order = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    class Meta:
+        verbose_name_plural = 'Service Categories'
+        ordering = ['order', 'name']
+
+    def __str__(self):
+        return self.name
+
+class Service(models.Model):
+    category = models.ForeignKey(ServiceCategory, on_delete=models.CASCADE, related_name='services')
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True, blank=True)
+    description = models.TextField()
+    short_description = models.CharField(max_length=300, blank=True)
+    price_from = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    duration = models.CharField(max_length=100, blank=True)
+    image = models.ImageField(upload_to='services/', blank=True, null=True)
+    is_featured = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    order = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    class Meta:
+        ordering = ['order', 'name']
+
+    def __str__(self):
+        return self.name
