e0098007e88bdcba960cd099018085772cdc783e
Jemimah Jemutai <jemutaijemimah@gmail.com>
2025-11-16 22:21:10 +0300

    Add CRUD views to gallery

--- gallery/views.py
+++ gallery/views.py
@@ -28,3 +28,56 @@ def gallery_detail(request, pk):
     """Display single gallery image details"""
     image = get_object_or_404(GalleryImage, pk=pk)
     return render(request, 'gallery/gallery_detail.html', {'image': image})
+
+# CRUD Operations
+def gallery_image_create(request):
+    """Upload a new gallery image"""
+    from django.contrib import messages
+    from django.shortcuts import redirect
+    
+    if request.method == 'POST':
+        from .forms import GalleryImageForm
+        form = GalleryImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Image uploaded successfully!')
+            return redirect('gallery:gallery_list')
+    else:
+        from .forms import GalleryImageForm
+        form = GalleryImageForm()
+    
+    return render(request, 'gallery/gallery_form.html', {'form': form})
+
+def gallery_image_update(request, pk):
+    """Update gallery image details"""
+    from django.contrib import messages
+    from django.shortcuts import redirect
+    
+    image = get_object_or_404(GalleryImage, pk=pk)
+    
+    if request.method == 'POST':
+        from .forms import GalleryImageForm
+        form = GalleryImageForm(request.POST, request.FILES, instance=image)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Image updated successfully!')
+            return redirect('gallery:gallery_detail', pk=image.pk)
+    else:
+        from .forms import GalleryImageForm
+        form = GalleryImageForm(instance=image)
+    
+    return render(request, 'gallery/gallery_form.html', {'form': form, 'image': image})
+
+def gallery_image_delete(request, pk):
+    """Delete a gallery image"""
+    from django.contrib import messages
+    from django.shortcuts import redirect
+    
+    image = get_object_or_404(GalleryImage, pk=pk)
+    
+    if request.method == 'POST':
+        image.delete()
+        messages.success(request, 'Image deleted successfully!')
+        return redirect('gallery:gallery_list')
+    
+    return render(request, 'gallery/gallery_confirm_delete.html', {'image': image})
