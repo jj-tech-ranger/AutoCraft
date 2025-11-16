11b501d61b4ea53266ed724cd8f648bbe5e6dfef
Jemimah Jemutai <jemutaijemimah@gmail.com>
2025-11-16 22:26:46 +0300

    Add CRUD URLs to bookings

--- bookings/urls.py
+++ bookings/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('create/<int:service_id>/', views.booking_create, name='booking_create'),
     path('<int:pk>/', views.booking_detail, name='booking_detail'),
     path('<int:pk>/cancel/', views.booking_cancel, name='booking_cancel'),
+        path('<int:pk>/update/', views.booking_update, name='booking_update'),
+    path('<int:pk>/delete/', views.booking_delete, name='booking_delete'),
 ]
